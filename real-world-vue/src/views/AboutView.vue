cada9ed308b5829047b692943661fcd522398205
Gabriel6966 <gabrielteren221@gmail.com>
2026-03-20 20:07:34 +0100

    TS changed and added export interface class

--- real-world-vue/src/views/AboutView.vue
+++ real-world-vue/src/views/AboutView.vue
@@ -3,5 +3,3 @@
     <h1>A sit for events to better the world</h1>
   </div>
 </template>
-
-
--- real-world-vue/src/views/NetworkError.vue
+++ real-world-vue/src/views/NetworkError.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="NetworkError">
-        <h1>Uh-Oh</h1>
-        <h3>Parece ser que estamos teniendo unos problemas internos de red,
-            porfavor danos un respiro y <a href="#" @click="$router.go(-1)">Pulse aqui</a>
-            para volver a intentarlo
-        </h3>
-    </div>
+  <div class="NetworkError">
+    <h1>Uh-Oh</h1>
+    <h3>
+      Parece ser que estamos teniendo unos problemas internos de red, porfavor danos un respiro y
+      <a href="#" @click="$router.go(-1)">Pulse aqui</a>
+      para volver a intentarlo
+    </h3>
+  </div>
 </template>
