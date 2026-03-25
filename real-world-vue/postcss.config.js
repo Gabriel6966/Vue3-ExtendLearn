eece50933fb632a32c452a90da853da2fb454742
Gabriel6966 <gabrielteren221@gmail.com>
2026-03-25 11:37:24 +0100

    Add postcss.config.js and debugging

---
+++ real-world-vue/postcss.config.js
@@ -0,0 +1,14 @@
+import postcssNested from 'postcss-nested'
+
+//Modelo de paquetes antiguo
+//module.exports={
+//  plugins:[
+//    require('postcss-nested')
+//  ]
+//}
+
+export default{
+  plugins:[
+    postcssNested
+  ]
+}
