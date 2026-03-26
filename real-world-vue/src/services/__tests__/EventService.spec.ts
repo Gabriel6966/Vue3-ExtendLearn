19f9a59c3dcaf85bdd028604aa719d93e27da4b9
Gabriel6966 <gabrielteren221@gmail.com>
2026-03-26 12:04:38 +0100

    Add correct mock to EvetnService

--- real-world-vue/src/services/__tests__/EventService.spec.ts
+++ real-world-vue/src/services/__tests__/EventService.spec.ts
@@ -17,7 +17,7 @@ vi.mock('axios', () => {
     },
   }
 })
-
+//Importamos despues de que la variable se ponga al mismo nivel que el mock
 import EventService from '@/services/EventService'
 
 describe('EventService', () => {
@@ -36,5 +36,25 @@ describe('EventService', () => {
 
     //Ejecutamos el events y le pasamos los argumentos(2,1) del service y ya rechazamos
     await expect(EventService.getEvents(2, 1)).rejects.toThrow()
+  })
+
+  //Comprobacion correcta
+  it('devolvemos llamada buena a la API', async () => {
+    //Metemos datos falsos
+    const eventos = [
+      { id: 1, title: 'Primero' },
+      { id: 2, title: 'Segundo' },
+    ]
+
+    sget.mockResolvedValueOnce({
+      data: eventos,
+    })
+
+    //Ejecucions
+    const respuesta = await EventService.getEvents(2, 1)
+
+    //Comprobacion
+    expect(respuesta.data).toEqual(eventos)
+    expect(respuesta.data).toHaveLength(2)
   })
 })
